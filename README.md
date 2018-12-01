5ab0fd72c54bc5d777fdebb89e86dc8a4a19330e
tarsillon1 <ricklypop@gmail.com>
2018-12-01 02:56:13 -0500

    Better header formatting.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Getting Started
+
+1. Install required dependencies.
+
+```bash
+$ npm i
+```
+
+2. Configure .env file. (Look at .sample.env for config options.)
+
+3. Run the script.
+
+```bash
+$ npm run start
+```
--- index.js
+++ index.js
@@ -29,49 +29,56 @@ async function getAll(type) {
 }
 
 function toCSV(entities) {
-  function title(col) {
-     var split = col.toLowerCase().split(' ');
-     for (var i = 0; i < split.length; i++) {
-         split[i] = split[i].charAt(0).toUpperCase() + split[i].substring(1);     
-     }
-     return split.join(' '); 
-  }
-
-  let array = typeof entities != "object" ? JSON.parse(entities) : entities;
   let header = [];
   entities.forEach(entity => {
     Object.keys(entity).forEach(key => {
-        if (header.indexOf(key) === -1) header.push(key);
+      if (header.indexOf(key) === -1) header.push(key);
     });
   });
 
   entities.forEach(entity => {
     header.forEach(col => {
-        if (!entity[col]) {
-          entity[col] = null;
-        }
+      if (!entity[col]) {
+        entity[col] = null;
+      }
     });
   });
 
-  let record = "";
-  entities.forEach(entity => {
-    let line = "";
-    header.forEach(col => {
-      if (typeof entity[col] === "string") entity[col] = `\"${entity[col]}\"`;
+  const replacer = (key, value) => {
+    if (!value) {
+      return null;
+    } else if (typeof value === "object") {
+      return JSON.stringify(value).replace(/"/g, "'");
+    } else if (typeof value === "string") {
+      return value.replace(/"/g, "'");
+    }
 
-      if (line != "") line += ",";
-      line += entity[col];
-    });
-    record += line + "\r\n";
+    return value;
+  };
+
+  let csv = entities.map(row =>
+    header.map(col => JSON.stringify(row[col], replacer)).join(",")
+  );
+
+  header.forEach((value, index) => {
+    var split = value
+      .replace(/_/g, " ")
+      .toLowerCase()
+      .split(" ");
+    for (var i = 0; i < split.length; i++) {
+      split[i] = split[i].charAt(0).toUpperCase() + split[i].substring(1);
+    }
+    header[index] = split.join(" ");
   });
 
-  header = title(header.join(", ").slice(0, -2).replace(/_/g, " "));
-  return `${header}\n${record}`;
+  csv.unshift(header.join(","));
+  csv = csv.join("\r\n");
+  return csv;
 }
 
 const PipelineDealsEntity = {
-  COMPANY: "companies",
-  USER: "users",
+  COMPANIES: "companies",
+  PEOPLE: "people",
   DEALS: "deals",
   COMPANIES_FIELDS: "admin/company_custom_field_labels",
   USERS_FIELDS: "admin/person_custom_field_labels",
@@ -103,23 +110,18 @@ const PipelineDealsEntity = {
       for (let [key, value] of Object.entries(entity)) {
         if (exclude.indexOf(key) !== -1) {
           delete entity[key];
-        } else if (value && typeof value === "object") {
-          Object.keys(value).forEach(subKey => {
-            entity[`${key}_${subKey}`] = value[subKey];
-          });
-          delete entity[key];
         }
       }
     });
   }
 
-  const companies = await getAll(PipelineDealsEntity.COMPANY);
-  const users = await getAll(PipelineDealsEntity.USER);
+  const companies = await getAll(PipelineDealsEntity.COMPANIES);
+  const users = await getAll(PipelineDealsEntity.PEOPLE);
   const deals = await getAll(PipelineDealsEntity.DEALS);
 
   [companies, users, deals].forEach(entities => filter(entities));
 
   fs.writeFileSync(__dirname + "/companies.csv", toCSV(companies));
-  fs.writeFileSync(__dirname + "/users.csv", toCSV(users));
+  fs.writeFileSync(__dirname + "/people.csv", toCSV(users));
   fs.writeFileSync(__dirname + "/deals.csv", toCSV(deals));
 })();
